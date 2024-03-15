b9ac0e3bc08bd3fa18c36aad21a54d6e13179b2c
Chandan-6 <khamitkarsaichandan1035@gmail.com>
2024-03-15 16:38:36 +0530

    added origin and optionssuccessstatus

--- server/server.js
+++ server/server.js
@@ -24,7 +24,10 @@ const port = 3010;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: 'https://nike-clone-frontend-two.vercel.app',
+    optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+}));
 
  
 app.use("/api/user", joinUsRoutes);
