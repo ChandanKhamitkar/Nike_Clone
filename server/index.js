b66f6462522b1f218f73d857d8811c0b3746298b
Chandan-6 <khamitkarsaichandan1035@gmail.com>
2024-03-16 08:26:19 +0530

    updated cors origin link

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://nike-clone-frontend-two.vercel.app',
+    origin: 'https://nike-clone-mern-chandank.vercel.app',
     optionsSuccessStatus: 200 // Some legacy browsers choke on 204
 }));
 
